61ef0d6977e0cc3b06fd79f1d9efa77262c1ee74
Izabel Eriksson <erikssonizabel@gmail.com>
2025-04-29 16:53:56 +0200

    test survey upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const surveyJson = {
+    elements: [{
+        name: "FirstName",
+        title: "Enter your first name:",
+        type: "text"
+    }, {
+        name: "LastName",
+        title: "Enter your last name:",
+        type: "text"
+    }]
+};
+
+const survey = new Survey.Model(surveyJson);
+
+document.addEventListener("DOMContentLoaded", function() {
+    survey.render(document.getElementById("surveyContainer"));
+});
